db346a74367c77137d9aedd7d310afa5b5c55204
Sprite Day <spriteday0@gmail.com>
2026-02-22 10:45:06 +0700

    Set up communication with manager task through a channel

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,17 +1,51 @@
+use bytes::Bytes;
 use mini_redis::client;
+use tokio::sync::mpsc;
+
+#[derive(Debug)]
+enum Command {
+    Get { key: String },
+    Set { key: String, value: Bytes },
+}
 
 #[tokio::main]
 async fn main() {
-    let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
 
-    let t1 = tokio::spawn(async {
-        let res = client.get("foo").await;
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+        while let Some(cmd) = rx.recv().await {
+            use Command::*;
+
+            match cmd {
+                Get { key } => {
+                    client.get(&key).await;
+                }
+                Set { key, value } => {
+                    client.set(&key, value).await;
+                }
+            }
+        }
     });
 
-    let t2 = tokio::spawn(async {
-        let res = client.set("foo", "bar".into()).await;
+    let t1 = tokio::spawn(async move {
+        let cmd = Command::Get {
+            key: "foo".to_string(),
+        };
+        tx.send(cmd).await.unwrap();
+    });
+
+    let t2 = tokio::spawn(async move {
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            value: "bar".into(),
+        };
+        tx2.send(cmd).await.unwrap();
     });
 
     t1.await.unwrap();
     t2.await.unwrap();
+    manager.await.unwrap();
 }
